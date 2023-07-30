cce2b7cd08b90fab8fcefe9ef562d6fc172e4869
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-07-30 12:09:07 -0400

    basic init, stats component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,41 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { StatsGrid, StatsGridProps } from '../components/Stats/Stats';
+
+const data: StatsGridProps = {
+  data: [
+    {
+      title: 'Block',
+      icon: 'block',
+      value: '13,456',
+      diff: 34,
+    },
+    {
+      title: 'Current fee',
+      icon: 'coin',
+      value: '4,145',
+      diff: -13,
+    },
+    {
+      title: 'AVG tx fee',
+      icon: 'discount',
+      value: '745',
+      diff: 18,
+    },
+    {
+      title: 'Mempool size',
+      icon: 'pool',
+      value: '188',
+      diff: -30,
+    },
+  ],
+};
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
+      <StatsGrid data={data.data} />
       <ColorSchemeToggle />
     </>
   );
