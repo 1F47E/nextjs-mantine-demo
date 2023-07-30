9cebbc0ca26156a017ece37e9d5d45a78739506d
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-07-30 12:18:23 -0400

    basic table

---
+++ components/Table/Table.tsx
@@ -0,0 +1,28 @@
+import { Table } from '@mantine/core';
+
+export function TableTransactions({ data }: { data: any[] }) {
+  const rows = data.map((element:any) => (
+    <tr key={element.name}>
+      <td>{element.position}</td>
+      <td>{element.name}</td>
+      <td>{element.symbol}</td>
+      <td>{element.mass}</td>
+    </tr>
+  ));
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Element position</th>
+          <th>Element name</th>
+          <th>Symbol</th>
+          <th>Atomic mass</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
+  );
+}
+
+export default TableTransactions;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { StatsGrid, StatsGridProps } from '../components/Stats/Stats';
+import { TableTransactions } from '../components/Table/Table';
 
 const data: StatsGridProps = {
   data: [
@@ -31,11 +32,21 @@ const data: StatsGridProps = {
   ],
 };
 
+// table data
+const elements = [
+  { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+  { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+  { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+  { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+  { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+];
+
 export default function HomePage() {
   return (
     <>
       <Welcome />
       <StatsGrid data={data.data} />
+      <TableTransactions data={elements} />
       <ColorSchemeToggle />
     </>
   );
